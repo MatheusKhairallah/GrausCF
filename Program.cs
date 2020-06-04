0a822d5061f6cb7c8f1f6c2b9901346b8a690db1
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:28:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um valor que corresponderá em °C e será convertido para °F");
+            Console.Write("°C: ");
+            double C=Convert.ToDouble(Console.ReadLine());
+            double CF= C * 1.8 + 32;
+            Console.Clear();
+            Console.WriteLine($"{C}°C em °F é={CF}");
+        }
+    }
+}
